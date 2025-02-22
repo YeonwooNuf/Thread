867fd6e6bff85004ccc3eab1ac9154d815640a22
YeonwooNuf <jangyw0927@naver.com>
2025-02-22 23:48:35 +0900

    // 기본 틀 생성
    엔티티 객체 생성

---
+++ src/main/java/com/example/board/model/Post.java
@@ -0,0 +1,9 @@
+package com.example.board.model;
+
+import java.time.ZonedDateTime;
+
+public class Post {
+    private Long id;
+    private String body;
+    private ZonedDateTime createdDateTime;
+}
