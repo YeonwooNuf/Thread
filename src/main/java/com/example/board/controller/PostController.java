2cba08a6b5239b8f517f2f23e20d71dca86d5a39
YeonwooNuf <jangyw0927@naver.com>
2025-02-22 23:57:50 +0900

    // Read api 설정

--- src/main/java/com/example/board/controller/PostController.java
+++ src/main/java/com/example/board/controller/PostController.java
@@ -1,8 +1,26 @@
 package com.example.board.controller;
 
+import com.example.board.model.Post;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PostController {
+
+    @GetMapping("/api/v1/posts")
+    public ResponseEntity<List<Post>> getPost() {
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1L, "Post 1", ZonedDateTime.now()));
+        posts.add(new Post(2L, "Post 2", ZonedDateTime.now()));
+        posts.add(new Post(3L, "Post 3", ZonedDateTime.now()));
+
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/example/board/model/Post.java
+++ src/main/java/com/example/board/model/Post.java
@@ -1,9 +1,61 @@
 package com.example.board.model;
 
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
 public class Post {
-    private Long id;
+    private Long postId;
     private String body;
     private ZonedDateTime createdDateTime;
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "postId=" + postId +
+                ", body='" + body + '\'' +
+                ", createdDateTime=" + createdDateTime +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(postId, post.postId) && Objects.equals(body, post.body) && Objects.equals(createdDateTime, post.createdDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(postId, body, createdDateTime);
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public ZonedDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(ZonedDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public Post(Long postId, String body, ZonedDateTime createdDateTime) {
+        this.postId = postId;
+        this.body = body;
+        this.createdDateTime = createdDateTime;
+    }
 }
